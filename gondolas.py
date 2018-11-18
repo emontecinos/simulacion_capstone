998e245059bf7e14440cd862f7354694d7f9794b
GONZALO NACRUR <gdnacrur@uc.cl>
2018-11-17 23:24:08 -0300

    swaps avances

--- gondolas.py
+++ gondolas.py
@@ -7,45 +7,60 @@ class Gondola:
         self.espacios = productos_esta_gondola(id) # [None for i in range(17)]
         self.largo = len(self.espacios) if self.espacios else 17
 
-    def solicitar_swap(self):
-        pass
-
-    def manejo_swap(self, producto_propio, largo_producto, producto_origen): #esta funcion maneja la solicitud de un swap
+    def manejo_swap(self, producto_propio, producto_origen, gondola_origen): #esta funcion maneja la solicitud de un swap
+        largo_producto = gondola_origen.espacios.count(producto_origen)
         largo_producto_viejo = self.espacios.count(producto_propio) # cuento cuantos espacios de la gondola estoy usando
         if largo_producto_viejo > largo_producto:
             return False
         else:
             if largo_producto_viejo == largo_producto:
-                self.swap([producto_propio for i in range(largo_producto_viejo)], producto_origen)
-                return [producto_propio for i in range(largo_producto_viejo)] # retorno la lista de slots a poner en la gondola que solicito el swap
+                self.swap(producto_propio, producto_origen, gondola_origen)
+                return True # retorno la lista de slots a poner en la gondola que solicito el swap
             else:
                 juntar = self.juntar_producto_adyacente(producto_propio, largo_producto)
-                if juntar[0]:
-                    self.swap(juntar[1], producto_origen)
-                    return juntar[1] ###### ver si el swap es con la misma gondola para no swapear dos veces
+                if juntar:
+                    self.swap(juntar, producto_origen, gondola_origen)
+                    return True ###### ver si el swap es con la misma gondola para no swapear dos veces
+                return False
 
 
 
-    def swap(self, productos_propios, producto_origen): #se cambian los espacios de los productos propios por el producto origen
-        pass
+    def swap(self, productos_propios, producto_origen, gondola_origen): #se cambian los espacios de los productos propios por el producto origen
+        i , j = self.donde_parte_y_donde_termina([producto_origen], gondola_origen)
+        lista_origen = gondola_origen.espacios[i:j+1]
+        g, h = self.donde_parte_y_donde_termina(productos_propios, self)
+        lista_propia = self.espacios[g: h + 1]
+        self.espacios[g: h + 1] = lista_origen
+        gondola_origen.espacios[i:j + 1] = lista_propia
+
+
+
+    @staticmethod
+    def donde_parte_y_donde_termina(productos, gondola):
+        i = 0
+        for espacio in gondola.espacios:
+            if espacio in productos:
+                break
+            else:
+                i += 1
+        j = i
+        for espacio in gondola.espacios[i:]:
+            if espacio not in productos:
+                j -= 1
+                break
+            else:
+                j += 1
+        return i,j
+
+
+
+
 
     def juntar_producto_adyacente(self, producto_propio, cantidad_necesitada): #ve si puede juntar el producto a cambiar con el del lado para poder hacer el swap
         '''retorna bool, lista --> el bool es de si se puede juntar
         y que calce la lista sería en caso de que se pueda donde se entrega
         la lista de espacios para darle a la otra gondola '''
-        i = 0
-        for espacio in self.espacios:
-            if espacio == producto_propio:
-                break
-            else:
-                i += 1
-        j = i
-        for espacio in self.espacios[i:]:
-            if espacio != producto_propio:
-                j -= 1
-                break
-            else:
-                j += 1
+        i , j = self.donde_parte_y_donde_termina([producto_propio], self)
         productos = {producto_propio}
         suma = self.espacios.count(producto_propio)
         intento_izq = self.intentar_combinaciones(True, i, j, suma, cantidad_necesitada, productos)
@@ -53,7 +68,6 @@ class Gondola:
         # productos_d = {producto_propio}
         # intento_der = self.intentar_combinaciones(False, i, j, suma, cantidad_necesitada, productos_d)
         if intento_izq[0]:
-            print("izq")
             return intento_izq[1]
         # elif intento_der[0]:
         #     print("derecha")
@@ -122,4 +136,8 @@ if __name__ == "__main__":
     slots = ["leche", "cereal", "queso", "trigo","trigo", "pan", "pan", "carne","carne","carne","carne", None, None, None, None, None, None]
     gondola = Gondola(0, 17)
     gondola.espacios = slots
-    print(gondola.juntar_producto_adyacente("queso", 5))
+    print(gondola.manejo_swap("cereal", "carne", gondola))
+    print(gondola.espacios)
+
+
+
