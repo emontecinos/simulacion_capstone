d1713d13d82a1f959617371d1f9e5e2b86cab21e
VicenteMelin <vtmelin@uc.cl>
2018-11-19 20:49:34 -0300

    sim2

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
     grafo.cargar_base("datos_nodos_new.csv")
     grafo.cargar_gondolas("distr_gondolas.csv")
     ######################## PARAMETROS DE LA SIMULACION ###########################
-    clientes_iniciales = 15
-    limite_boletas = 100
-    iteraciones_por_config = 1
-    veces_que_busca_mejora = 10
+    clientes_iniciales = 150
+    limite_boletas = 200
+    iteraciones_por_config = 4
+    veces_que_busca_mejora = 20
     #################################################################################
     simulacion = Simulacion(grafo)
     estadisticas_prom = dict()
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     estadisticas_prom["distancia_media_recorrida"] = 0
 
 ########################################################################################################################
-    print("ESTADISTICAS INICIALES")
+    # print("ESTADISTICAS INICIALES")
     for i in range(20):
         estadisticas_iniciales = simulacion.run(clientes_iniciales, limite_boletas)
         for estadistica in estadisticas_iniciales.items():
@@ -41,13 +41,13 @@ if __name__ == "__main__":
                     estadisticas_prom[estadistica[0]] += estadistica[1] / (20 * clientes_iniciales)
                 else:
                     estadisticas_prom[estadistica[0]] += estadistica[1] / 20
-    print("Tardó: {}".format(time.time()-t1))
-    print("_______________________")
-
-    # for i in grafo.nodos.items():
-    #     print(i[0], i[1].veces_visitado)
-    ########################################################################################################################
-    # matar = 0
+    with open("Swaps_hechos.csv", 'w') as file:
+        for x in estadisticas_prom.items():
+            file.write("{}:, {} \n".format(x[0], x[1]))
+    with open('Swaps_hechos.csv', 'a') as file:
+        file.write("Prod1, Prod2 \n")
+    # print("Tardó: {}".format(time.time()-t1))
+    # print("_______________________")
     lista_swaps = swapity_swap(simulacion.grafo)
     lista = []
     for lista_fam in lista_swaps.values():
@@ -60,13 +60,13 @@ if __name__ == "__main__":
         #     print("-----------------------NO SE ENCONTRO MEJORA-----------------------------------")
         #     break
         grafo_iteracion_actual = deepcopy(grafo)
-        print("ESTASITICAS A SUPERAR")
-        print("prob total : {}".format(estadisticas_prom["probabilidad_total"]))
-        print("utilidad_espontanea: {}".format(estadisticas_prom["utilidad_espontanea"]))
-        print("numero medio de compras: {}".format(estadisticas_prom["numero_medio_compras_espontaneas"]))
-        print("______________________________________________________")
-
-        print("############# LOS SWAGPS ################")
+        # print("ESTASITICAS A SUPERAR")
+        # print("prob total : {}".format(estadisticas_prom["probabilidad_total"]))
+        # print("utilidad_espontanea: {}".format(estadisticas_prom["utilidad_espontanea"]))
+        # print("numero medio de compras: {}".format(estadisticas_prom["numero_medio_compras_espontaneas"]))
+        # print("______________________________________________________")
+        #
+        # print("############# LOS SWAGPS ################")
         for _ in range(len(lista_swaps)):
             swap = lista_swaps.pop(0)
             nro_swap = 1
@@ -81,8 +81,8 @@ if __name__ == "__main__":
                 estadisticas_nuevas["numero_medio_compras_espontaneas"] = 0
                 estadisticas_nuevas["top_productos_espontaneos"] = 0
                 estadisticas_nuevas["distancia_media_recorrida"] = 0
+                revision_mitad = True
                 for nro_iter in range(iteraciones_por_config):
-                    revision_mitad = True
                     estadisticas_iteracion = simulacion.run(clientes_iniciales, limite_boletas)
                     for estadistica in estadisticas_iteracion.items():
                         if type(estadistica[1]) is not list:
@@ -96,26 +96,24 @@ if __name__ == "__main__":
                         if estadisticas_prom["utilidad_espontanea"] * 1.05/2 < estadisticas_nuevas["utilidad_espontanea"] \
                                 and estadisticas_prom["numero_medio_compras_espontaneas"] * 1.05/2 < estadisticas_nuevas[
                                      "numero_medio_compras_espontaneas"]:
-                            print("Estadisticas a la mitad:")
-                            print("p total {}".format(estadisticas_nuevas["probabilidad_total"]))
-                            print("ut espontanea {}".format(estadisticas_nuevas["utilidad_espontanea"]))
-                            print("nume medio {}".format(estadisticas_nuevas["numero_medio_compras_espontaneas"]))
+                            pass
+                            # print("Estadisticas a la mitad:")
+                            # print("p total {}".format(estadisticas_nuevas["probabilidad_total"]))
+                            # print("ut espontanea {}".format(estadisticas_nuevas["utilidad_espontanea"]))
+                            # print("nume medio {}".format(estadisticas_nuevas["numero_medio_compras_espontaneas"]))
                         else:
-                            print("-----No es un buen candidato----- ")
-                            print("_______________________")
-
+                            # print("-----No es un buen candidato----- ")
+                            # print("_______________________")
                             buen_candidato = False
                             break
                 mejor = False
                 if buen_candidato:
-                    print("Es un buen candidato")
-                    print("Se probo un swap de {} por {}".format(swap[1], swap[2]))
-                    print("prob total : {}".format(estadisticas_nuevas["probabilidad_total"]))
+                    # print("Es un buen candidato")
+                    # print("Se probo un swap de {} por {}".format(swap[1], swap[2]))
+                    # print("prob total : {}".format(estadisticas_nuevas["probabilidad_total"]))
                     print("utilidad_espontanea: {}".format(estadisticas_nuevas["utilidad_espontanea"]))
                     print("numero medio de compras: {}".format(estadisticas_nuevas["numero_medio_compras_espontaneas"]))
-                    if ((estadisticas_prom["probabilidad_total"] * 1.05 < estadisticas_nuevas["probabilidad_total"] and estadisticas_prom["utilidad_espontanea"] * 1.05 < estadisticas_nuevas["utilidad_espontanea"]) or
-                        (estadisticas_prom["probabilidad_total"] * 1.05 < estadisticas_nuevas["probabilidad_total"] and estadisticas_prom["numero_medio_compras_espontaneas"] < 1.05 * estadisticas_nuevas["numero_medio_compras_espontaneas"]) or
-                        (estadisticas_prom["utilidad_espontanea"] * 1.05 < estadisticas_nuevas["utilidad_espontanea"] and estadisticas_prom["numero_medio_compras_espontaneas"] < 1.05 * estadisticas_nuevas["numero_medio_compras_espontaneas"])):
+                    if estadisticas_prom["utilidad_espontanea"] < estadisticas_nuevas["utilidad_espontanea"] and estadisticas_prom["numero_medio_compras_espontaneas"] < estadisticas_nuevas["numero_medio_compras_espontaneas"]:
                             mejor = True
                     if mejor:
                         swaps_hechos.append((swap[1], swap[2]))
@@ -123,14 +121,22 @@ if __name__ == "__main__":
                         print('La ESTADISTICA ANTERIOR MEJORO')
                         print("_______________________")
                         simulacion.grafo.guardar_gondolas()
+                        with open('Swaps_hechos.csv', 'a') as file:
+                            file.write("{}, {} \n".format(swap[1], swap[2]))
                         break
                     else:
-                        print("Era un buen candidato pero no lo suficiente")
-                        print("_______________________")
+                        pass
+                        # print("Era un buen candidato pero no lo suficiente")
+                        # print("_______________________")
 
                 else:
-                    print("Mal candidato")
-                    print("_______________________")
+                    pass
+                    # print("Mal candidato")
+                    # print("_______________________")
         grafo = grafo_iteracion_actual
+    with open('Swaps_hechos.csv', 'a') as file:
+        file.write("ESTADISTICAS FINALES \n")
+        for i in estadisticas_prom.items():
+            file.write("{} : {} \n".format(i[0], i[1]))
 
 
